951449be27e7076c25bd07f86267de34f0ee31ae
Rahul Nisanth <133683060+Rahulnisanth@users.noreply.github.com>
2024-06-24 20:48:36 +0530

    first-commit

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import { Box } from "@chakra-ui/react";
+
+const Footer = () => (
+  <Box textAlign="center" p="5" color="gray.600">
+    Made by Team Project Inc &mdash; SIET.
+    <br />
+    &copy; 2024 &mdash; SpatialNest.in
+  </Box>
+);
+
+export default Footer;
